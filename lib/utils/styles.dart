e485ca110fee52699e44b635cfa0a72afda5d95a
Jamsheer <jamsheermoidu@gmail.com>
2022-07-11 16:57:36 +0530

    settings page and animations completed

---
+++ lib/utils/styles.dart
@@ -0,0 +1,11 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_app/utils/colors.dart';
+
+import 'constants.dart';
+
+class AppStyles {
+  static final appBarTextStyle = GoogleFonts.montserrat(
+    fontSize: Constants.appBarTextSize,
+    color: AppColors.primaryColor,
+  );
+}
